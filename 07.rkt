4305f1b5e939de7f857f389c1b89e1f143df9adb
Johan Viklund <johan.viklund@nbis.se>
2022-12-07 20:55:38 +0100

    A pure solution to the problem
    Just one hack, added a "cd .." to the end of the input file.
    
    I wonder if it would be better with a solution that recursed everytime a
    "cd <dir>" is found and then return the size of the dir when "cd .." is
    detected.

--- 07.rkt
+++ 07.rkt
@@ -18,14 +18,47 @@
                [(string=? first "dir") (list "dir" (car rest))]
                [else                   (list "file" (string->number first))])))
 
-(define state (make-hash))
+; State is like this
+; (hash "current-dir" . "Dir"
+;       "<-dir->"     . number)
 
-(for ([l (in-lines in)])
-     (let ([r (parse-line l)])
-       (case (car r)
-         [("ls")   (displayln "ls")] ; really a no-op
-         [("cd")   (displayln "cd")]
-         [("dir")  (displayln "dir")]
-         [("file") (displayln (cadr r))]
-         [else    (displayln "WTF")]
-          )))
+(define (clean-dir dir)
+    (string-join (string-split dir "/" #:trim? #t #:repeat? #t) "/"))
+
+(define (add-file-size state num)
+    (hash-update state (hash-ref state "current-dir") (curry + num)))
+
+(define (up-dir state)
+  (let* ([current-dir (hash-ref state "current-dir")]
+         [dir-size    (hash-ref state current-dir)]
+         [new-dir     (string-join (drop-right (string-split current-dir "/" #:trim? #t #:repeat? #t) 1) "/")]
+         [new-state   (hash-update state new-dir (curry + dir-size))])
+    (hash-set new-state "current-dir" new-dir)))
+
+(define (enter-dir state dir)
+  (let* ([current-dir (hash-ref state "current-dir")]
+         [new-dir (clean-dir (string-join (list current-dir dir) "/"))]
+         [st      (hash-set state new-dir 0)])
+    (hash-set st "current-dir" new-dir)))
+
+(define (perform-command command state)
+  (case (car command)
+    [("ls" "dir")   state] ; no-ops, just return the state
+    [("file")       (add-file-size state (cadr command))]
+    [("cd")         (if (equal? (cadr command) "..") (up-dir state) (enter-dir state (cadr command)))]))
+
+
+(define res
+  (for/fold
+    ([st (hash "current-dir" "")])
+    ([l (in-lines in)])
+    (perform-command (parse-line l) st)))
+
+(sum (filter (lambda (x) (and (number? x) (< x 100000))) (hash-values res)))
+
+(define free-space (- 70000000 (hash-ref res "")))
+
+(define (last-elem l)
+  (if (null? (cdr l)) (car l) (last-elem (cdr l))))
+
+(last-elem (filter (lambda (x) (< 30000000 (+ free-space x))) (sort (filter (curry number?) (hash-values res)) >)))
